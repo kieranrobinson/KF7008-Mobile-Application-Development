0688da9e6c7b6c268338035a08f3518b743ea294
Kieran Robinson <robinson.kieran@outlook.com>
2023-02-02 00:33:48 +0000

    Added main menu activity with buttons

---
+++ app/src/main/java/uk/co/kieranrobinson/MainActivity.java
@@ -0,0 +1,41 @@
+package uk.co.kieranrobinson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openAddMemoryActivity(View view){
+        Context context = getApplicationContext();
+        int toastDuration = Toast.LENGTH_SHORT;
+        CharSequence toastText = "Feature not implemented yet";
+        Toast toast = Toast.makeText(context, toastText, toastDuration);
+        toast.show();
+    }
+
+    public void openViewMemoriesActivity(View view){
+        Context context = getApplicationContext();
+        int toastDuration = Toast.LENGTH_SHORT;
+        CharSequence toastText = "Feature not implemented yet";
+        Toast toast = Toast.makeText(context, toastText, toastDuration);
+        toast.show();
+    }
+
+    public void openViewMapActivity(View view){
+        Context context = getApplicationContext();
+        int toastDuration = Toast.LENGTH_SHORT;
+        CharSequence toastText = "Feature not implemented yet";
+        Toast toast = Toast.makeText(context, toastText, toastDuration);
+        toast.show();
+    }
+}
