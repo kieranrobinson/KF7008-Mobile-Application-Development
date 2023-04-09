441a1e6e5ebc5b95d7311a75085f9755704b943d
Kieran Robinson <robinson.kieran@outlook.com>
2023-04-09 22:26:00 +0100

    Added listener to check when memory is clicked in list, and open new intent

--- app/src/main/java/uk/co/kieranrobinson/MemoriesActivity.java
+++ app/src/main/java/uk/co/kieranrobinson/MemoriesActivity.java
@@ -2,7 +2,10 @@ package uk.co.kieranrobinson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,12 +13,16 @@ import java.util.ArrayList;
 
 public class MemoriesActivity extends AppCompatActivity {
     private SQLiteDB sqliteDB;
+    ListView list;
+    private Intent selectedActivityIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memories);
 
+
+        selectedActivityIntent = new Intent(this, Memory.class);
         sqliteDB = new SQLiteDB(MemoriesActivity.this);
 
         setupListview();
@@ -23,10 +30,22 @@ public class MemoriesActivity extends AppCompatActivity {
 
     private void setupListview(){
         ArrayList<String> listMemoryNames = sqliteDB.getAllMemoryNames();
-        ListView list = findViewById(R.id.listMemories);
+        list = findViewById(R.id.listMemories);
 
         ArrayAdapter<String> arrayAdapter;
         arrayAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listMemoryNames);
         list.setAdapter(arrayAdapter);
+
+        list.setOnItemClickListener(entryPressed);
     }
+
+    private AdapterView.OnItemClickListener entryPressed = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            String selectedItem = (String)list.getItemAtPosition(i);
+            startActivity(selectedActivityIntent);
+        }
+    };
+
+
 }
---
+++ app/src/main/java/uk/co/kieranrobinson/Memory.java
@@ -0,0 +1,14 @@
+package uk.co.kieranrobinson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Memory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory);
+    }
+}
