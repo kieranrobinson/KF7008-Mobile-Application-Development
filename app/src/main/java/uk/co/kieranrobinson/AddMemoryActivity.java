d5bfcb97b6f821c86a2ec22370b20c10710b1d77
Kieran Robinson <robinson.kieran@outlook.com>
2023-04-19 02:30:28 +0100

    Added code to retrieve longitude and latitude when adding new memory, and print values to commandline

--- app/src/main/java/uk/co/kieranrobinson/AddMemoryActivity.java
+++ app/src/main/java/uk/co/kieranrobinson/AddMemoryActivity.java
@@ -1,18 +1,31 @@
 package uk.co.kieranrobinson;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 
 import java.io.Console;
 
@@ -27,6 +40,9 @@ public class AddMemoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_memory);
 
         sqliteDB = new SQLiteDB(AddMemoryActivity.this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
     }
 
     public void addMemory(View view){
@@ -39,10 +55,26 @@ public class AddMemoryActivity extends AppCompatActivity {
         getLastLocation();
     }
 
+    //Missing permission warning suppressed as if statement checks for this already
+    @SuppressLint("MissingPermission")
     private void getLastLocation(){
         if(checkPermissions()){
             if(isLocationEnabled()){
                 //TODO: Do location logic
+                fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if(location == null){
+                            System.out.println("Was null: Requesting update");
+                            requestLocationUpdates();
+                        } else {
+                            System.out.println("Latitude: " + location.getLatitude());
+                            System.out.println("Longitude: " + location.getLongitude());
+                        }
+                    }
+                });
+
                 System.out.println("LOCATION ENABLED");
             }
         }
@@ -50,6 +82,27 @@ public class AddMemoryActivity extends AppCompatActivity {
             requestPermissions();
         }
     }
+
+    //Missing permission warning suppressed as if statement checks for this already
+    @SuppressLint("MissingPermission")
+    public void requestLocationUpdates(){
+        LocationRequest locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000)
+                .setIntervalMillis(500)
+                .setMaxUpdateDelayMillis(1000)
+                .build();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            Location last = locationResult.getLastLocation();
+            System.out.println("Location callback set longitude to " + last.getLongitude());
+            System.out.println("Location callback set latitude to " + last.getLatitude());
+        }
+    };
 
     private boolean isLocationEnabled(){
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
