dc85b0854961efdf3d2a7a48a276c48a1d00908f
Kieran Robinson <robinson.kieran@outlook.com>
2023-04-29 02:09:00 +0100

    Added search filter within the memories ListView

--- app/src/main/java/uk/co/kieranrobinson/MemoriesActivity.java
+++ app/src/main/java/uk/co/kieranrobinson/MemoriesActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,19 +17,24 @@ import java.util.ArrayList;
 public class MemoriesActivity extends AppCompatActivity {
     private SQLiteDB sqliteDB;
     ListView list;
+    EditText editTextSearch;
     private Intent selectedActivityIntent;
     ArrayList<Integer> listMemoryIDs = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memories);
 
-
+        editTextSearch = findViewById(R.id.editTextSearch);
         selectedActivityIntent = new Intent(this, Memory.class);
         sqliteDB = new SQLiteDB(MemoriesActivity.this);
 
+        setupListSearch();
         setupListview();
+
+
         getSupportActionBar().setTitle("Memories");
     }
 
@@ -34,13 +42,31 @@ public class MemoriesActivity extends AppCompatActivity {
         ArrayList<String> listMemoryNames = sqliteDB.getAllMemoryNames();
         list = findViewById(R.id.listMemories);
 
-        ArrayAdapter<String> arrayAdapter;
         arrayAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, listMemoryNames);
         list.setAdapter(arrayAdapter);
 
         listMemoryIDs = sqliteDB.getAllMemoryID();
 
         list.setOnItemClickListener(entryPressed);
+    }
+
+    private void setupListSearch(){
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //Unused
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (MemoriesActivity.this).arrayAdapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //Unused
+            }
+        });
     }
 
     private AdapterView.OnItemClickListener entryPressed = new AdapterView.OnItemClickListener() {
